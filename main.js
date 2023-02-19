dc62a48ef75f14fe9e4f5f15248b8c72314b4f49
Carlos Nieto <cnietocu@gmail.com>
2023-02-19 15:14:48 -0500

    product list merge and HTML from array in JS

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const burguerIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const cartIcon = document.querySelector('.navbar-shopping-cart');
 const shoppingCart = document.querySelector('.cart-detail');
-
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burguerIcon.addEventListener('click', toggleMobileMenu);
@@ -12,31 +12,97 @@ cartIcon.addEventListener('click', toggleCart);
 
 function toggleDesktopMenu() {
     const isCartClosed = shoppingCart.classList.contains('inactive');
-
     if (!isCartClosed) {
         shoppingCart.classList.add('inactive');
     }
-    
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
     const isCartClosed = shoppingCart.classList.contains('inactive');
-
     if (!isCartClosed) {
         shoppingCart.classList.add('inactive');
     }
-
     mobileMenu.classList.toggle('inactive');
 }
 
 function toggleCart() {
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
-    
     if (!isMobileMenuClosed) {
         mobileMenu.classList.add('inactive');
     }
-
     shoppingCart.classList.toggle('inactive');
-
 }
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&w=400'
+});
+productList.push({
+    name: 'Laptop',
+    price: 550,
+    image: 'https://images.pexels.com/photos/326508/pexels-photo-326508.jpeg?auto=compress&cs=tinysrgb&w=1600'
+});
+productList.push({
+    name: 'Phone',
+    price: 340,
+    image: 'https://images.pexels.com/photos/1092644/pexels-photo-1092644.jpeg?auto=compress&cs=tinysrgb&w=1600'
+});
+productList.push({
+    name: 'Couch',
+    price: 220,
+    image: 'https://images.pexels.com/photos/3757055/pexels-photo-3757055.jpeg?auto=compress&cs=tinysrgb&w=1600'
+});
+productList.push({
+    name: 'Cushion',
+    price: 30,
+    image: 'https://images.pexels.com/photos/1248583/pexels-photo-1248583.jpeg?auto=compress&cs=tinysrgb&w=1600'
+});
+productList.push({
+    name: 'Lamp',
+    price: 40,
+    image: 'https://images.pexels.com/photos/1112598/pexels-photo-1112598.jpeg?auto=compress&cs=tinysrgb&w=1600'
+});
+
+
+function productsRendering(arr) {
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+productsRendering(productList);
+
